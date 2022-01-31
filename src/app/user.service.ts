1979f7a8cd7620fcfc0404f17bbc5c51d97eed4d
yadavdeepak486 <33428677+yadavdeepak486@users.noreply.github.com>
2022-01-31 12:19:43 +0530

    done some

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -8,83 +8,83 @@ export class UserService {
 
   backendurltest = 'https://demo.rishtaguru.com/api';
   backendurl = 'http://localhost:6789/api';
-  getvalue:string = ""
+  getvalue: string = ""
   constructor(public http: HttpClient) {
     console.log("constructor hit");
   }
 
-  login(data:any) {
-    return this.http.post(`${this.backendurl}/user/login`,data);
+  login(data: any) {
+    return this.http.post(`${this.backendurl}/user/login`, data);
   }
 
   getmyprofile() {
     let header = new HttpHeaders({
       'Content-Type': 'application/json',
-      'auth-token':"Bearer "+localStorage.getItem('auth-token'),
+      'auth-token': "Bearer " + localStorage.getItem('auth-token'),
     })
-    return this.http.get(`${this.backendurl}/user/myprofile`,{
+    return this.http.get(`${this.backendurl}/user/myprofile`, {
       headers: header,
     });
   }
 
-  getcalllogs(id:any){
+  getcalllogs(id: any) {
     return this.http.get(`${this.backendurl}/admin/getdetailofonenumber/${id}`);
   }
 
-  getoutgoingcallcount(id:any){
+  getoutgoingcallcount(id: any) {
     return this.http.get(`${this.backendurl}/admin/outgoingcallcount/${id}`);
   }
 
-  getincomingcallcount(id:any){
+  getincomingcallcount(id: any) {
     return this.http.get(`${this.backendurl}/admin/incomingcallcount/${id}`);
   }
 
-  getallcallcount(id:any){
+  getallcallcount(id: any) {
     return this.http.get(`${this.backendurl}/admin/totalcallcount/${id}`);
   }
 
-  getmissedcallcount(id:any){
+  getmissedcallcount(id: any) {
     return this.http.get(`${this.backendurl}/admin/missedcallcount/${id}`);
   }
 
-  getreceivedcalls(id:any){
+  getreceivedcalls(id: any) {
     return this.http.get(`${this.backendurl}/admin/getreceivedcalls/${id}`);
   }
 
-  getallplans(){
+  getallplans() {
     return this.http.get(`${this.backendurl}/admin/allplan`)
   }
 
-  getincomingcalldetails(id:any){
+  getincomingcalldetails(id: any) {
     return this.http.get(`${this.backendurl}/admin/getdetailincoming/${id}`)
   }
 
-  getmissedcalldetails(id:any){
+  getmissedcalldetails(id: any) {
     return this.http.get(`${this.backendurl}/admin/missedcalls/${id}`)
   }
 
-  getallcalldetails(id:any){
+  getallcalldetails(id: any) {
     return this.http.get(`${this.backendurl}/admin/totalcalldetails/${id}`)
   }
 
-  viewonecalldetails(id:any){
+  viewonecalldetails(id: any) {
     return this.http.get(`${this.backendurl}/admin/viewonecdrreport/${id}`)
   }
 
 
-  getchatwithuser(id:any) {
+  getchatwithuser(id: any) {
     return this.http.get(`${this.backendurl}/admin/allchatwithuser/${id}`);
   }
 
-  sendchat(data:any) {
+  sendchat(data: any) {
     return this.http.post(`${this.backendurl}/admin/addchat`, data);
   }
 
-  markasread(id:any) {
+  markasread(id: any) {
     return this.http.get(`${this.backendurl}/admin/markasread/${id}`);
   }
 
-  unreadmessages(id:any) {
+  unreadmessages(id: any) {
     return this.http.get(`${this.backendurl}/admin/unreadmessages/${id}`);
   }
 }
